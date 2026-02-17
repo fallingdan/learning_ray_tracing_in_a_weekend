b8a06bd37568875b8644bd6af70039d41998d8bd
Dan Wagner <onewaylimited@gmail.com>
2026-02-17 15:44:52 -0600

    Working on vec3 implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use indicatif::ProgressBar;
-mod vector3;
+mod vec3;
 
 fn main() {
     // Image
@@ -9,20 +9,20 @@ fn main() {
     // Render
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
-    let progressBar: ProgressBar = ProgressBar::new(image_height.into());
+    let progress_bar: ProgressBar = ProgressBar::new(image_height.into());
 
     for j in 0..image_height {
-        progressBar.inc(1);
+        progress_bar.inc(1);
         for i in 0..image_width {
             let r: f64 = (i as f64) / (image_width - 1) as f64;
             let g: f64 = (j as f64) / (image_height - 1) as f64;
             let b: f64 = 0.0;
 
-            let intR: i32 = (255.999 * r) as i32;
-            let intG: i32 = (255.999 * g) as i32;
-            let intB: i32 = (255.999 * b) as i32;
+            let int_r: i32 = (255.999 * r) as i32;
+            let int_g: i32 = (255.999 * g) as i32;
+            let int_b: i32 = (255.999 * b) as i32;
 
-            print!("{} {} {}\n", intR, intG, intB);
+            print!("{} {} {}\n", int_r, int_g, int_b);
         }
     }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Div, Mul, Neg};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
@@ -7,29 +7,123 @@ pub struct Vec3 {
 
 impl Vec3 {
     pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
-        Self { e: [e1, e2, e3] }
+        Self { e: [e0, e1, e2] }
     }
 
-    pub fn x() -> f64 {
-        e[0]
+    pub fn x(self) -> f64 {
+        self.e[0]
     }
 
-    pub fn y() -> f64 {
-        e[1]
+    pub fn y(self) -> f64 {
+        self.e[1]
     }
 
-    pub fn z() -> f64 {
-        e[2]
+    pub fn z(self) -> f64 {
+        self.e[2]
+    }
+
+    pub fn dotproduct(v: &Vec3, u: &Vec3) -> f64 {
+        (v.e[0] * u.e[0]) + (v.e[1] * u.e[1]) + (v.e[2] * u.e[2])
     }
 }
 
 impl Add for Vec3 {
     type Output = Vec3;
     fn add(self, rhs: Self) -> Self::Output {
-        Self::new(
-            self::x() + rhs::x(),
-            self::y() + rhs::y(),
-            self::z() + rhs::z(),
-        )
+        Self::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Self::new(-self.x(), -self.y(), -self.z())
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let actual: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+
+        let expected: [f64; 3] = [1.0, 2.0, 3.0];
+
+        assert!(actual.e == expected)
+    }
+
+    #[test]
+    fn test_x() {
+        let subject: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+
+        assert!(subject.x() == 1.0);
+    }
+
+    #[test]
+    fn test_y() {
+        let subject: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+
+        assert!(subject.y() == 2.0);
+    }
+
+    #[test]
+    fn test_z() {
+        let subject: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+
+        assert!(subject.z() == 3.0);
+    }
+
+    #[test]
+    fn test_add() {
+        let lhs: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+        let rhs: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+
+        let expected: [f64; 3] = [2.0, 4.0, 6.0];
+        let actual: Vec3 = lhs + rhs;
+
+        assert!(actual.e == expected)
+    }
+
+    #[test]
+    fn test_neg() {
+        let subject = Vec3::new(1.0, 1.0, 1.0);
+        let actual = -subject;
+
+        assert!([-1.0, -1.0, -1.0] == actual.e);
+    }
+
+    #[test]
+    fn test_dotproduct() {
+        let left = Vec3::new(2.0, 3.0, 4.0);
+        let right = Vec3::new(3.0, 4.0, 5.0);
+
+        let expected: f64 = 38.0;
+
+        let actual = Vec3::dotproduct(&left, &right);
+
+        assert!(actual == expected);
+    }
+
+    #[test]
+    fn test_mul() {
+        let subject = Vec3::new(2.0, 3.0, 4.0);
+        let right = Vec3::new(3.0, 4.0, 5.0);
+
+        let expected: [f64; 3] = [6.0, 12.0, 20.0];
+
+        let actual = subject * right;
+
+        assert!(actual.e == expected);
     }
 }
